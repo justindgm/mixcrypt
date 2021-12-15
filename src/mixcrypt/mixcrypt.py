c02c2170da0ae6778748bd28df92bad13ec9b372
Justin Mack <justindmack@gmail.com>
2021-12-15 14:21:07 -0500

    update mixcrypt

--- src/mixcrypt/mixcrypt.py
+++ src/mixcrypt/mixcrypt.py
@@ -10,7 +10,7 @@ rsa_public_key, rsa_private_key = rsa.newkeys(3072)
 
 def encrypt(tensor: torch.tensor):
     signs, exponents, mantissas = float_to_binary(np.asarray(tensor))
-    encrypted_signs = [phe_public_key.encrypt(val.item()) for val in signs.flatten()]
-    encrypted_mantissas = [phe_public_key.encrypt(val.item()) for val in mantissas.flatten()]
-    encrypted_exponents = [int.from_bytes(bytestring, 'little') for bytestring in [rsa.encrypt(val.item().to_bytes(2, byteorder='little'), rsa_public_key) for val in exponents.flatten()]]
+    encrypted_signs = [int.from_bytes(bytestring, 'little') for bytestring in [rsa.encrypt(val.item().to_bytes(1, byteorder='little'), rsa_public_key) for val in signs.flatten()]]
+    encrypted_exponents = [phe_public_key.encrypt(val.item()) for val in exponents.flatten()]
+    encrypted_mantissas = [int.from_bytes(bytestring, 'little') for bytestring in [rsa.encrypt(val.item().to_bytes(6, byteorder='little'), rsa_public_key) for val in mantissas.flatten()]]
     return encrypted_signs, encrypted_mantissas, encrypted_exponents
