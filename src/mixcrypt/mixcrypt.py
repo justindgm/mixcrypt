91ae0a13f1c544203dee27746bf625c58f86ef97
Justin Mack <justindmack@gmail.com>
2021-12-15 12:26:39 -0500

    update mixcrypt

--- src/mixcrypt/mixcrypt.py
+++ src/mixcrypt/mixcrypt.py
@@ -1,22 +1,16 @@
 import phe
 import rsa
 import torch
-import cupy as cp
+import numpy as np
 
 from mixcrypt.util import float_to_binary
 
 phe_public_key, phe_private_key = phe.generate_paillier_keypair()
 rsa_public_key, rsa_private_key = rsa.newkeys(3072)
 
-t = torch.rand((3,3,2))
-
 def encrypt(tensor: torch.tensor):
-    signs, exponents, mantissas = float_to_binary(cp.asarray(tensor))
-    for val in mantissas.flatten():
-        print(val, type(val))
-    
-    encrypted_mantissas = [phe_public_key.encrypt(val) for val in mantissas.flatten()]
-    encrypted_exponents = [rsa.encrypt(val, rsa_public_key) for val in exponents.flatten()]
-    return 0
-
-encrypt(t)
+    signs, exponents, mantissas = float_to_binary(np.asarray(tensor))
+    encrypted_signs = [phe_public_key.encrypt(val.item()) for val in signs.flatten()]
+    encrypted_mantissas = [phe_public_key.encrypt(val.item()) for val in mantissas.flatten()]
+    encrypted_exponents = [int.from_bytes(bytestring, 'little') for bytestring in [rsa.encrypt(val.item().to_bytes(2, byteorder='little'), rsa_public_key) for val in exponents.flatten()]]
+    return encrypted_signs, encrypted_mantissas, encrypted_exponents
