9ec6cb5019cf0532ef44f02ee12047f7d3049397
Justin Mack <justindmack@gmail.com>
2021-12-15 11:06:02 -0500

    update mixcrypt

---
+++ mixcrypt/__about__.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import, division, print_function
+
+__all__ = [
+    "__title__", "__summary__", "__uri__", "__version__", "__author__",
+    "__email__", "__license__", "__copyright__",
+]
+
+__title__ = "mixcrypt"
+__summary__ = "Arbitrary Precision Encryption for Floating Point Values"
+__uri__ = "https://github.com/data61/python-paillier"
+
+__version__ = "0.0.0"
+
+__author__ = "Justin Mack"
+__email__ = "justindmack@gmail.com"
+__license__ = "MIT"
+__copyright__ = "Copyright 2022 {0}".format(__author__)
